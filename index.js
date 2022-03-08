beb95442b6e4421315abfc196d27859fa9255d64
seetharampullela <seetharampullela@gmail.com>
2022-03-08 16:29:47 +0000

    Congrats card tests reactjs work2

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div class="bgContainer">
+    <h1> Congratulations </h1>
+    <div class="cardContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="image"
+      />
+      <h1>Kiran V</h1>
+      <p> Vishnu Institute of Computer Education and Technology</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
